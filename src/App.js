27f7ebc9b1be3db774c00718b14f5689ef4e6ec9
Emílio de Aguiar <85713266+emiliodaf@users.noreply.github.com>
2022-09-25 19:40:17 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import Movie from './components/Movie';
+import { useEffect, useState } from 'react';
+import { Featured_API } from './config/Key';
+
+
+function App() {
+
+  const [movies, setMovies ] = useState([]);
+  const [ searchTerm, setSearchTerm ] = useState('')
+
+  useEffect(() =>{
+      fetch(Featured_API)
+      .then(response => response.json())
+      .then(data => setMovies(data.results))
+
+  },[])
+
+
+   const handleOnsubmit = (e) => {
+         e.preventDefault()
+
+      if(searchTerm)   {
+
+        fetch(`https://api.themoviedb.org/3/search/movie?&api_key=4f790ac4d97736336cc69ca0f10e2d4a&query=${searchTerm}`)
+        .then(response => response.json())
+        .then(data => setMovies(data.results))
+
+        setSearchTerm('');
+      }
+
+   };
+
+   const handleOnchange = (e) => {
+         setSearchTerm(e.target.value)
+   }
+  
+  return (
+     <>
+    <header>
+      <form onSubmit={handleOnsubmit}>  
+      <input className='search' 
+      type="search" 
+      placeholder='search...'
+      value={searchTerm}
+      onChange={handleOnchange}></input>
+      </form> 
+   </header> 
+    <div className='movie-container'>
+      { 
+        movies.map(movie => (
+        <Movie key={movie.id} {...movie} />
+      ))}
+    </div>
+    </>
+
+  )
+}
+
+export default App;
